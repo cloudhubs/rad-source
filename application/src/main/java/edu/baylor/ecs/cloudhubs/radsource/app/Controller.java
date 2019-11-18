7e9e52ec566d3e7f4f89d68cb59051171ef86e87
Dipta Das <dipta670@gmail.com>
2019-11-18 03:22:37 -0600

    Update controller

--- application/src/main/java/edu/baylor/ecs/cloudhubs/radsource/app/Controller.java
+++ application/src/main/java/edu/baylor/ecs/cloudhubs/radsource/app/Controller.java
@@ -6,6 +6,8 @@ import edu.baylor.ecs.cloudhubs.radsource.context.RadSourceResponseContext;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+
 @RestController
 @AllArgsConstructor
 public class Controller {
@@ -14,7 +16,7 @@ public class Controller {
     @CrossOrigin(origins = "*")
     @RequestMapping(path = "/", method = RequestMethod.POST, produces = "application/json; charset=UTF-8", consumes = {"text/plain", "application/*"})
     @ResponseBody
-    public RadSourceResponseContext getRadSourceResponseContext(@RequestBody RadSourceRequestContext request) {
+    public RadSourceResponseContext getRadSourceResponseContext(@RequestBody RadSourceRequestContext request) throws IOException {
         return radSourceService.generateRadSourceResponseContext(request);
     }
 }
